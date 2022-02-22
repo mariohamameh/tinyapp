4fa614f476ce8fe2258b15076ab8431aa0599cd4
mariohamameh <mariohamameh97@gmail.com>
2022-02-22 18:32:53 +0000

    added generate random string

--- express_server.js
+++ express_server.js
@@ -2,7 +2,22 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+const bodyParser = require("body-parser");
+
+function generateRandomString() {
+    const minVal = 35 ** 5;
+    const randomVal = Math.floor(Math.random() * minVal) + minVal;
+    return randomVal.toString(35);
+    
+}
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.set("view engine", "ejs");
+
+app.post("/urls", (req, res) => {
+    console.log(req.body);  // Log the POST request body to the console
+    res.send('ok');         // Respond with 'Ok' (we will replace this)
+});
 
 const urlDatabase = {
     "b2xVn2": "http://www.lighthouselabs.ca",
@@ -18,10 +33,14 @@ app.get("/urls", (req, res) => {
     res.render("urls_show", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+    res.render("urls_new");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
     const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase/* What goes here? */ };
     res.render("urls_show", templateVars);
-  });
+});
 
 app.get("/", (req, res) => {
     res.send("Hello!");
